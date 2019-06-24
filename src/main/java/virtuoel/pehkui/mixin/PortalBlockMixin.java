98cd73bd4e2543138bd631564f191a8413115097
Virtuoel <Virtuoel@users.noreply.github.com>
2019-06-24 18:28:56 +0300

    Tweak Portal mixin to use outline shape

--- src/main/java/virtuoel/pehkui/mixin/PortalBlockMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/PortalBlockMixin.java
@@ -1,8 +1,6 @@
 package virtuoel.pehkui.mixin;
 
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -11,18 +9,12 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.PortalBlock;
 import net.minecraft.entity.Entity;
-import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.world.World;
 
 @Mixin(PortalBlock.class)
 public abstract class PortalBlockMixin extends Block
 {
-	@Shadow @Final static VoxelShape X_SHAPE;
-	@Shadow @Final static VoxelShape Z_SHAPE;
-	
 	private PortalBlockMixin()
 	{
 		super(null);
@@ -31,7 +23,7 @@ public abstract class PortalBlockMixin extends Block
 	@Inject(at = @At("HEAD"), method = "onEntityCollision", cancellable = true)
 	private void onOnEntityCollision(BlockState blockState_1, World world_1, BlockPos blockPos_1, Entity entity_1, CallbackInfo info)
 	{
-		if(!entity_1.getBoundingBox().intersects((blockState_1.get(Properties.HORIZONTAL_AXIS) == Direction.Axis.Z ? Z_SHAPE : X_SHAPE).getBoundingBox().offset(blockPos_1)))
+		if(!entity_1.getBoundingBox().intersects(blockState_1.getOutlineShape(world_1, blockPos_1).getBoundingBox().offset(blockPos_1)))
 		{
 			info.cancel();
 		}
