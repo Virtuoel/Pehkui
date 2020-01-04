fe2b21ce666d70881d81f53adbf9c7a8aad47d1c
Virtuoel <Virtuoel@users.noreply.github.com>
2020-01-05 00:08:20 +0300

    Fix scaled Item Frame rendering

---
+++ src/main/java/virtuoel/pehkui/mixin/ItemFrameEntityRendererMixin.java
@@ -0,0 +1,20 @@
+package virtuoel.pehkui.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.client.render.entity.ItemFrameEntityRenderer;
+import net.minecraft.entity.decoration.ItemFrameEntity;
+import net.minecraft.util.math.Vec3d;
+
+@Mixin(ItemFrameEntityRenderer.class)
+public class ItemFrameEntityRendererMixin
+{
+	@Inject(at = @At("RETURN"), method = "getPositionOffset", cancellable = true)
+	private void onGetPositionOffset(ItemFrameEntity entity, float tickDelta, CallbackInfoReturnable<Vec3d> info)
+	{
+		info.setReturnValue(info.getReturnValue().multiply(-5.0D / 48.0D, ((double) entity.getHorizontalFacing().getOffsetY()) * 0.125D, -5.0D / 48.0D));
+	}
+}
