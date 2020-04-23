0fd864f571b9ee8db1be2537296b7891115f90a6
Virtuoel <Virtuoel@users.noreply.github.com>
2020-04-23 04:12:02 +0300

    Fix 1.15.2 compat

--- src/main/java/virtuoel/pehkui/mixin/client/compat116plus/EntityRendererMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/client/compat116plus/EntityRendererMixin.java
@@ -1,4 +1,4 @@
-package virtuoel.pehkui.mixin.client;
+package virtuoel.pehkui.mixin.client.compat116plus;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,7 +11,7 @@ import virtuoel.pehkui.api.ScaleData;
 @Mixin(EntityRenderer.class)
 public abstract class EntityRendererMixin
 {
-	@Redirect(method = "renderLabelIfPresent(Lnet/minecraft/entity/Entity;Ljava/lang/String;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;getHeight()F"))
+	@Redirect(method = "renderLabelIfPresent(Lnet/minecraft/entity/Entity;Lnet/minecraft/text/Text;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;I)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;getHeight()F"))
 	private float renderLabelIfPresentGetHeightProxy(Entity entity)
 	{
 		return (entity.getHeight() / ScaleData.of(entity).getScale());
