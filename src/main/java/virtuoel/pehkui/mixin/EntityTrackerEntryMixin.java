512258102c97c3c3f335b3d5aaabd5f4f78aaf64
Virtuoel <Virtuoel@users.noreply.github.com>
2020-04-24 16:01:03 +0300

    Improve motion sync of smaller entities

--- src/main/java/virtuoel/pehkui/mixin/EntityTrackerEntryMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/EntityTrackerEntryMixin.java
@@ -4,7 +4,9 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import io.netty.buffer.Unpooled;
@@ -31,5 +33,13 @@ public abstract class EntityTrackerEntryMixin
 		{
 			sendSyncPacket(new CustomPayloadS2CPacket(Pehkui.SCALE_PACKET, data.toPacketByteBuf(new PacketByteBuf(Unpooled.buffer()).writeUuid(entity.getUuid()))));
 		}
+	}
+	
+	@ModifyConstant(method = "tick", constant = @Constant(doubleValue = 7.62939453125E-6D))
+	private double tickModifyMinimumSquaredDistance(double value)
+	{
+		final double scale = ScaleData.of(entity).getScale();
+		
+		return scale < 1.0D ? value * scale * scale : value;
 	}
 }
