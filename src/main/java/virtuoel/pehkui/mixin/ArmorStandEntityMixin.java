30afae954dece3a54b26083b421b64aa7ba6f7bb
Virtuoel <Virtuoel@users.noreply.github.com>
2020-06-07 23:30:07 +0300

    Fix armor stand not scaling properly

---
+++ src/main/java/virtuoel/pehkui/mixin/ArmorStandEntityMixin.java
@@ -0,0 +1,19 @@
+package virtuoel.pehkui.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import net.minecraft.entity.decoration.ArmorStandEntity;
+
+@Mixin(ArmorStandEntity.class)
+public abstract class ArmorStandEntityMixin extends EntityMixin
+{
+	@ModifyArg(method = "getDimensions", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/EntityDimensions;scaled(F)Lnet/minecraft/entity/EntityDimensions;"))
+	private float onGetDimensionsModifyScale(float value)
+	{
+		final float scale = pehkui_scaleData.getScale();
+		
+		return scale != 1.0F ? value * scale : value;
+	}
+}
