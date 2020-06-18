e666d1725ba64d0caab2dd1731c38d1cc4c26cfe
Virtuoel <Virtuoel@users.noreply.github.com>
2020-06-19 00:19:55 +0300

    Cleanup

--- src/main/java/virtuoel/pehkui/mixin/client/compat115plus/PlayerEntityRendererMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/client/compat115plus/PlayerEntityRendererMixin.java
@@ -6,20 +6,13 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.client.network.AbstractClientPlayerEntity;
-import net.minecraft.client.render.entity.LivingEntityRenderer;
 import net.minecraft.client.render.entity.PlayerEntityRenderer;
-import net.minecraft.client.render.entity.model.PlayerEntityModel;
 import net.minecraft.util.math.Vec3d;
 import virtuoel.pehkui.api.ScaleData;
 
 @Mixin(PlayerEntityRenderer.class)
-public abstract class PlayerEntityRendererMixin extends LivingEntityRenderer<AbstractClientPlayerEntity, PlayerEntityModel<AbstractClientPlayerEntity>>
+public abstract class PlayerEntityRendererMixin
 {
-	private PlayerEntityRendererMixin()
-	{
-		super(null, null, 0);
-	}
-	
 	@Inject(at = @At("RETURN"), method = "getPositionOffset", cancellable = true)
 	private void onGetPositionOffset(AbstractClientPlayerEntity entity, float tickDelta, CallbackInfoReturnable<Vec3d> info)
 	{
