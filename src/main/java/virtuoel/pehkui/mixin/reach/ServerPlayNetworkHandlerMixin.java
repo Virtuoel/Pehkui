4300efbbf78b8694c8a03130a0eda22d0c950bd7
Virtuoel <Virtuoel@users.noreply.github.com>
2020-08-06 00:32:01 +0300

    Remove redundant reach config check

--- src/main/java/virtuoel/pehkui/mixin/reach/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/reach/ServerPlayNetworkHandlerMixin.java
@@ -1,18 +1,12 @@
 package virtuoel.pehkui.mixin.reach;
 
-import java.util.Optional;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
-import com.google.gson.JsonElement;
-import com.google.gson.JsonPrimitive;
-
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
-import virtuoel.pehkui.api.PehkuiConfig;
 import virtuoel.pehkui.util.ScaleUtils;
 
 @Mixin(ServerPlayNetworkHandler.class)
@@ -25,18 +19,7 @@ public class ServerPlayNetworkHandlerMixin
 	{
 		final float scale = ScaleUtils.getReachScale(player);
 		
-		if (scale > 1.0F)
-		{
-			if (Optional.ofNullable(PehkuiConfig.DATA.get("scaledReach"))
-				.filter(JsonElement::isJsonPrimitive).map(JsonElement::getAsJsonPrimitive)
-				.filter(JsonPrimitive::isBoolean).map(JsonPrimitive::getAsBoolean)
-				.orElse(true))
-			{
-				return scale * scale * value;
-			}
-		}
-		
-		return value;
+		return scale > 1.0F ? scale * scale * value : value;
 	}
 	
 	@ModifyConstant(method = "onPlayerInteractEntity", constant = @Constant(doubleValue = 36.0D))
@@ -44,17 +27,6 @@ public class ServerPlayNetworkHandlerMixin
 	{
 		final float scale = ScaleUtils.getReachScale(player);
 		
-		if (scale > 1.0F)
-		{
-			if (Optional.ofNullable(PehkuiConfig.DATA.get("scaledReach"))
-				.filter(JsonElement::isJsonPrimitive).map(JsonElement::getAsJsonPrimitive)
-				.filter(JsonPrimitive::isBoolean).map(JsonPrimitive::getAsBoolean)
-				.orElse(true))
-			{
-				return scale * scale * value;
-			}
-		}
-		
-		return value;
+		return scale > 1.0F ? scale * scale * value : value;
 	}
 }
