5db13b050f557ada0d6b602dc8484dd951ccd0fc
Virtuoel <Virtuoel@users.noreply.github.com>
2020-08-27 20:56:00 +0300

    Fix items merging incorrectly when resized

---
+++ src/main/java/virtuoel/pehkui/mixin/ItemEntityMixin.java
@@ -0,0 +1,20 @@
+package virtuoel.pehkui.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+import net.minecraft.entity.ItemEntity;
+import virtuoel.pehkui.util.ScaleUtils;
+
+@Mixin(ItemEntity.class)
+public class ItemEntityMixin
+{
+	@ModifyConstant(method = "tryMerge", constant = @Constant(doubleValue = 0.5D))
+	private double tryMergeModifyWidth(double value)
+	{
+		final float scale = ScaleUtils.getWidthScale(this);
+		
+		return scale != 1.0F ? scale * value : value;
+	}
+}
