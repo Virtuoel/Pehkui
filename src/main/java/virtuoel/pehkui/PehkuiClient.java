367ea97189c4f59ab7bb7cd0a49b91b5c180e0f7
Virtuoel <Virtuoel@users.noreply.github.com>
2020-12-26 00:18:42 +0300

    Only use V1 networking now that it's backported

--- src/main/java/virtuoel/pehkui/PehkuiClient.java
+++ src/main/java/virtuoel/pehkui/PehkuiClient.java
@@ -1,56 +1,47 @@
 package virtuoel.pehkui;
 
 import java.util.UUID;
-import java.util.function.BiConsumer;
 
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.loader.api.FabricLoader;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.Entity;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.network.PacketByteBuf;
 import net.minecraft.util.Identifier;
 import virtuoel.pehkui.api.ScaleData;
 import virtuoel.pehkui.api.ScaleRegistries;
-import virtuoel.pehkui.util.FabricApiCompatibility;
 
 public class PehkuiClient implements ClientModInitializer
 {
 	@Override
 	public void onInitializeClient()
 	{
-		final BiConsumer<MinecraftClient, PacketByteBuf> handler = (client, buf) ->
-		{
-			final UUID uuid = buf.readUuid();
-			final Identifier typeId = buf.readIdentifier();
-			
-			final CompoundTag scaleData = ScaleData.fromPacketByteBufToTag(buf);
-			
-			if (!ScaleRegistries.SCALE_TYPES.containsKey(typeId))
-			{
-				return;
-			}
-			
-			client.execute(() ->
-			{
-				for (final Entity e : client.world.getEntities())
-				{
-					if (e.getUuid().equals(uuid))
-					{
-						ScaleRegistries.SCALE_TYPES.get(typeId).getScaleData(e).fromTag(scaleData);
-						break;
-					}
-				}
-			});
-		};
-		
 		if (FabricLoader.getInstance().isModLoaded("fabric-networking-api-v1"))
 		{
-			FabricApiCompatibility.Client.registerScalePacket(handler);
-		}
-		else if (FabricLoader.getInstance().isModLoaded("fabric-networking-v0"))
-		{
-			FabricApiCompatibility.Client.registerV0ScalePacket(handler);
+			ClientPlayNetworking.registerGlobalReceiver(Pehkui.SCALE_PACKET, (client, handler, buf, sender) ->
+			{
+				final UUID uuid = buf.readUuid();
+				final Identifier typeId = buf.readIdentifier();
+				
+				final CompoundTag scaleData = ScaleData.fromPacketByteBufToTag(buf);
+				
+				if (!ScaleRegistries.SCALE_TYPES.containsKey(typeId))
+				{
+					return;
+				}
+				
+				client.execute(() ->
+				{
+					for (final Entity e : client.world.getEntities())
+					{
+						if (e.getUuid().equals(uuid))
+						{
+							ScaleRegistries.SCALE_TYPES.get(typeId).getScaleData(e).fromTag(scaleData);
+							break;
+						}
+					}
+				});
+			});
 		}
 		else
 		{
