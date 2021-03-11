d33b83cc57b244f0da89106395d5739905750040
Virtuoel <Virtuoel@users.noreply.github.com>
2021-03-12 02:50:11 +0300

    Fix certain types of item reach not being scaled
    Closes #51

---
+++ src/main/java/virtuoel/pehkui/mixin/reach/ItemMixin.java
@@ -0,0 +1,28 @@
+package virtuoel.pehkui.mixin.reach;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Constant;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.world.RaycastContext;
+import net.minecraft.world.World;
+import virtuoel.pehkui.util.ScaleUtils;
+
+@Mixin(Item.class)
+public class ItemMixin
+{
+	@ModifyConstant(method = "raycast", constant = @Constant(doubleValue = 5.0D))
+	private static double raycastModifyMultiplier(double value, World world, PlayerEntity player, RaycastContext.FluidHandling fluidHandling)
+	{
+		final float scale = ScaleUtils.getReachScale(player);
+		
+		if (scale != 1.0F)
+		{
+			return value * scale;
+		}
+		
+		return value;
+	}
+}
