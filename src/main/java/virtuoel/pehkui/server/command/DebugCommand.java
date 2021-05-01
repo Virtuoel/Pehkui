72dbd312f441d67d3cc0e78d3537a75534ed9562
Virtuoel <Virtuoel@users.noreply.github.com>
2021-05-01 17:49:46 +0300

    Cleanup

--- src/main/java/virtuoel/pehkui/server/command/DebugCommand.java
+++ src/main/java/virtuoel/pehkui/server/command/DebugCommand.java
@@ -28,70 +28,6 @@ import net.minecraft.util.math.Direction;
 
 public class DebugCommand
 {
-	private static final Collection<UUID> MARKED_UUIDS = new HashSet<>();
-	private static final Collection<String> MARKED_USERNAMES = new HashSet<>();
-	
-	public static boolean unmarkEntityForScaleReset(final Entity entity, final NbtCompound nbt)
-	{
-		if (entity instanceof PlayerEntity && MARKED_USERNAMES.remove(((PlayerEntity) entity).getGameProfile().getName().toLowerCase(Locale.ROOT)))
-		{
-			return true;
-		}
-		
-		return nbt.containsUuid("UUID") && MARKED_UUIDS.remove(nbt.getUuid("UUID"));
-	}
-	
-	private static final List<EntityType<? extends Entity>> TYPES = Arrays.asList(
-		EntityType.ZOMBIE,
-		EntityType.CREEPER,
-		EntityType.END_CRYSTAL,
-		EntityType.BLAZE
-	);
-	
-	private static int runTests(CommandContext<ServerCommandSource> context) throws CommandSyntaxException
-	{
-		ServerPlayerEntity player = context.getSource().getPlayer();
-		
-		Direction dir = player.getHorizontalFacing();
-		Direction opposite = dir.getOpposite();
-		
-		Direction left = dir.rotateYCounterclockwise();
-		Direction right = dir.rotateYClockwise();
-		
-		int distance = 4;
-		int spacing = 2;
-		
-		int width = ((TYPES.size() - 1) * (spacing + 1)) + 1;
-		
-		BlockPos start = player.getBlockPos().offset(dir, distance).offset(left, width / 2);
-		
-		BlockPos.Mutable mut = start.mutableCopy();
-		
-		ServerWorld w = player.getServerWorld();
-		
-		for (EntityType<?> t : TYPES)
-		{
-			w.setBlockState(mut, Blocks.POLISHED_ANDESITE.getDefaultState());
-			final Entity e = t.create(w);
-			
-			e.updatePositionAndAngles(mut.getX() + 0.5, mut.getY() + 1, mut.getZ() + 0.5, opposite.asRotation(), 0);
-			e.refreshPositionAndAngles(mut.getX() + 0.5, mut.getY() + 1, mut.getZ() + 0.5, opposite.asRotation(), 0);
-			e.setHeadYaw(opposite.asRotation());
-			
-			e.addScoreboardTag("pehkui");
-			
-			w.spawnEntity(e);
-			
-			mut.move(right, spacing + 1);
-		}
-		
-		// TODO set command block w/ entity to void and block destroy under player pos
-		
-		context.getSource().sendFeedback(new LiteralText("Tests succeeded."), false);
-		
-		return 1;
-	}
-	
 	public static void register(final CommandDispatcher<ServerCommandSource> commandDispatcher, final boolean dedicated)
 	{
 		register(commandDispatcher);
@@ -156,5 +92,69 @@ public class DebugCommand
 				)
 			);
 		}
+	}
+	
+	private static final Collection<UUID> MARKED_UUIDS = new HashSet<>();
+	private static final Collection<String> MARKED_USERNAMES = new HashSet<>();
+	
+	public static boolean unmarkEntityForScaleReset(final Entity entity, final NbtCompound nbt)
+	{
+		if (entity instanceof PlayerEntity && MARKED_USERNAMES.remove(((PlayerEntity) entity).getGameProfile().getName().toLowerCase(Locale.ROOT)))
+		{
+			return true;
+		}
+		
+		return nbt.containsUuid("UUID") && MARKED_UUIDS.remove(nbt.getUuid("UUID"));
+	}
+	
+	private static final List<EntityType<? extends Entity>> TYPES = Arrays.asList(
+		EntityType.ZOMBIE,
+		EntityType.CREEPER,
+		EntityType.END_CRYSTAL,
+		EntityType.BLAZE
+	);
+	
+	private static int runTests(CommandContext<ServerCommandSource> context) throws CommandSyntaxException
+	{
+		ServerPlayerEntity player = context.getSource().getPlayer();
+		
+		Direction dir = player.getHorizontalFacing();
+		Direction opposite = dir.getOpposite();
+		
+		Direction left = dir.rotateYCounterclockwise();
+		Direction right = dir.rotateYClockwise();
+		
+		int distance = 4;
+		int spacing = 2;
+		
+		int width = ((TYPES.size() - 1) * (spacing + 1)) + 1;
+		
+		BlockPos start = player.getBlockPos().offset(dir, distance).offset(left, width / 2);
+		
+		BlockPos.Mutable mut = start.mutableCopy();
+		
+		ServerWorld w = player.getServerWorld();
+		
+		for (EntityType<?> t : TYPES)
+		{
+			w.setBlockState(mut, Blocks.POLISHED_ANDESITE.getDefaultState());
+			final Entity e = t.create(w);
+			
+			e.updatePositionAndAngles(mut.getX() + 0.5, mut.getY() + 1, mut.getZ() + 0.5, opposite.asRotation(), 0);
+			e.refreshPositionAndAngles(mut.getX() + 0.5, mut.getY() + 1, mut.getZ() + 0.5, opposite.asRotation(), 0);
+			e.setHeadYaw(opposite.asRotation());
+			
+			e.addScoreboardTag("pehkui");
+			
+			w.spawnEntity(e);
+			
+			mut.move(right, spacing + 1);
+		}
+		
+		// TODO set command block w/ entity to void and block destroy under player pos
+		
+		context.getSource().sendFeedback(new LiteralText("Tests succeeded."), false);
+		
+		return 1;
 	}
 }
