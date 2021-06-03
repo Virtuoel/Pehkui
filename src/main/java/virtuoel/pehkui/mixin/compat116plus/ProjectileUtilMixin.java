4c0e4131730c2dea68a6b78eab06512308667542
Virtuoel <Virtuoel@users.noreply.github.com>
2021-06-03 05:51:28 +0300

    1.17-pre4

--- src/main/java/virtuoel/pehkui/mixin/compat116plus/ProjectileUtilMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/compat116plus/ProjectileUtilMixin.java
@@ -5,7 +5,6 @@ import java.util.function.Predicate;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.projectile.ProjectileUtil;
@@ -29,21 +28,5 @@ public class ProjectileUtilMixin
 		}
 		
 		return box;
-	}
-	
-	@Redirect(method = "getEntityCollision", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;getBoundingBox()Lnet/minecraft/util/math/Box;"))
-	private static Box getEntityCollisionModifyGetBoundingBox(Entity obj)
-	{
-		final float width = ScaleUtils.getWidthScale(obj);
-		final float height = ScaleUtils.getHeightScale(obj);
-		
-		if (width != 1.0F || height != 1.0F)
-		{
-			final double scaledWidth = (width * 0.30000001192092896D) - 0.30000001192092896D;
-			final double scaledHeight = (height * 0.30000001192092896D) - 0.30000001192092896D;
-			return obj.getBoundingBox().expand(scaledWidth, scaledHeight, scaledWidth);
-		}
-		
-		return obj.getBoundingBox();
 	}
 }
---
+++ src/main/java/virtuoel/pehkui/mixin/compat117plus/ProjectileUtilMixin.java
@@ -0,0 +1,34 @@
+package virtuoel.pehkui.mixin.compat117plus;
+
+import java.util.function.Predicate;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.projectile.ProjectileUtil;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import virtuoel.pehkui.util.ScaleUtils;
+
+@Mixin(ProjectileUtil.class)
+public class ProjectileUtilMixin
+{
+	@Redirect(method = "method_37226", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;getBoundingBox()Lnet/minecraft/util/math/Box;"))
+	private static Box getEntityCollisionModifyGetBoundingBox(Entity obj, World world, Entity except, Vec3d vec3d, Vec3d vec3d2, Box box, Predicate<Entity> predicate, float value)
+	{
+		final float width = ScaleUtils.getWidthScale(obj);
+		final float height = ScaleUtils.getHeightScale(obj);
+		
+		if (width != 1.0F || height != 1.0F)
+		{
+			final double scaledWidth = (width * value) - value;
+			final double scaledHeight = (height * value) - value;
+			return obj.getBoundingBox().expand(scaledWidth, scaledHeight, scaledWidth);
+		}
+		
+		return obj.getBoundingBox();
+	}
+}
