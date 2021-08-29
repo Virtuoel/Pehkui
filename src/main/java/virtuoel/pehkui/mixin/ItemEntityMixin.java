81a1a1d2047ded3ded8200988d0ee7da8c1fa246
Virtuoel <Virtuoel@users.noreply.github.com>
2021-08-29 06:56:29 +0300

    Potentially fixes #169

--- src/main/java/virtuoel/pehkui/mixin/ItemEntityMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/ItemEntityMixin.java
@@ -1,21 +1,31 @@
 package virtuoel.pehkui.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.ItemEntity;
+import net.minecraft.util.math.Box;
 import virtuoel.pehkui.util.ScaleUtils;
 
 @Mixin(ItemEntity.class)
 public class ItemEntityMixin
 {
-	@ModifyConstant(method = "tryMerge", constant = @Constant(doubleValue = 0.5D))
-	private double tryMergeModifyWidth(double value)
+	@ModifyArg(method = "tryMerge", index = 1, at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getEntitiesByClass(Ljava/lang/Class;Lnet/minecraft/util/math/Box;Ljava/util/function/Predicate;)Ljava/util/List;"))
+	private Box tryMergeModifyBounds(Box box)
 	{
-		final float scale = ScaleUtils.getBoundingBoxWidthScale((Entity) (Object) this);
+		final ItemEntity self = (ItemEntity) (Object) this;
+		final Box bounds = self.getBoundingBox();
 		
-		return scale != 1.0F ? scale * value : value;
+		final double xExpand = box.getXLength() - bounds.getXLength();
+		final double yExpand = box.getYLength() - bounds.getYLength();
+		final double zExpand = box.getZLength() - bounds.getZLength();
+		final float widthScale = ScaleUtils.getBoundingBoxWidthScale(self);
+		final float heightScale = ScaleUtils.getBoundingBoxHeightScale(self);
+		
+		return bounds.expand(
+			widthScale != 1.0F ? widthScale * xExpand : xExpand,
+			heightScale != 1.0F ? heightScale * yExpand : yExpand,
+			widthScale != 1.0F ? widthScale * zExpand : zExpand);
 	}
 }
