8f086a9359bc6744f28479f528491b9775730749
Virtuoel <Virtuoel@users.noreply.github.com>
2021-08-30 04:23:48 +0300

    Fix scaled max health rendering as double the amount
    Fixes first part of #176

--- src/main/java/virtuoel/pehkui/mixin/client/compat115minus/InGameHudMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/client/compat115minus/InGameHudMixin.java
@@ -5,7 +5,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
@@ -22,7 +22,7 @@ public abstract class InGameHudMixin
 	@Shadow(remap = false) @Final @Mutable
 	MinecraftClient field_2035; // UNMAPPED_FIELD
 	
-	@ModifyVariable(method = MixinConstants.RENDER_STATUS_BARS, at = @At("STORE"), remap = false)
+	@ModifyArg(method = MixinConstants.RENDER_STATUS_BARS, index = 0, at = @At(value = "INVOKE", target = "Ljava/lang/Math;max(FF)F"))
 	private float onRenderStatusBars(float value)
 	{
 		final float healthScale = ScaleUtils.getHealthScale(method_1737(), field_2035.getTickDelta());
