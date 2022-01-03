4f7eae16dc34480e51a268c82f206317df9bbb5a
Virtuoel <Virtuoel@users.noreply.github.com>
2022-01-03 03:31:25 +0300

    Fix chest closing if player is too far (1.16.x-)

---
+++ src/main/java/virtuoel/pehkui/mixin/compat116minus/BarrelBlockEntityMixin.java
@@ -0,0 +1,63 @@
+package virtuoel.pehkui.mixin.compat116minus;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.block.entity.ChestBlockEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import virtuoel.pehkui.util.ScaleUtils;
+import virtuoel.pehkui.util.ViewerCountingBlockEntityExtensions;
+
+@Mixin(ChestBlockEntity.class)
+public class BarrelBlockEntityMixin implements ViewerCountingBlockEntityExtensions
+{
+	@Unique
+	float viewerSearchRange = 5.0F;
+	
+	@Shadow(remap = false)
+	int field_17583;
+	
+	@Override
+	public float pehkui_getViewerSearchRange()
+	{
+		return viewerSearchRange;
+	}
+	
+	@Inject(at = @At("HEAD"), method = "method_5435(Lnet/minecraft/class_1657;)V", remap = false)
+	private void onOnInvOpen(PlayerEntity player, CallbackInfo info)
+	{
+		if (field_17583 < 0)
+		{
+			field_17583 = 0;
+			
+			viewerSearchRange = 5.0F;
+		}
+		
+		final float scale = ScaleUtils.getBlockReachScale(player);
+		
+		if (scale != 1.0F)
+		{
+			final float nextRange = 5.0F * scale;
+			
+			if (nextRange > viewerSearchRange)
+			{
+				viewerSearchRange = nextRange;
+			}
+		}
+	}
+	
+	@Inject(at = @At("HEAD"), method = "method_5432(Lnet/minecraft/class_1657;)V", remap = false)
+	private void onOnInvClose(PlayerEntity player, CallbackInfo info)
+	{
+		if (field_17583 <= 1)
+		{
+			field_17583 = 1;
+			
+			viewerSearchRange = 5.0F;
+		}
+	}
+}
