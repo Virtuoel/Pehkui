f0e59928bb0c61971050c585652ce3553fab0b13
Virtuoel <Virtuoel@users.noreply.github.com>
2022-01-06 21:22:56 +0300

    Cleanup summoned Vex scaling

--- src/main/java/virtuoel/pehkui/mixin/compat116plus/SummonVexGoalMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/compat116plus/SummonVexGoalMixin.java
@@ -1,27 +1,24 @@
 package virtuoel.pehkui.mixin.compat116plus;
 
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 import net.minecraft.entity.Entity;
-import net.minecraft.entity.mob.EvokerEntity;
+import net.minecraft.entity.mob.VexEntity;
 import virtuoel.pehkui.util.ScaleUtils;
 
 @Mixin(targets = "net.minecraft.entity.mob.EvokerEntity$SummonVexGoal")
 public class SummonVexGoalMixin
 {
-	@Shadow @Final @Mutable
-	EvokerEntity field_7267;
-	
 	@ModifyArg(method = "castSpell()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/world/ServerWorld;spawnEntityAndPassengers(Lnet/minecraft/entity/Entity;)V"))
-	private Entity castSpellSpawnEntityAndPassengersProxy(Entity vexEntity)
+	private Entity castSpellSpawnEntityAndPassengersProxy(Entity entity)
 	{
-		ScaleUtils.loadScale(vexEntity, field_7267);
+		if (entity instanceof VexEntity)
+		{
+			ScaleUtils.loadScale(entity, ((VexEntity) entity).getOwner());
+		}
 		
-		return vexEntity;
+		return entity;
 	}
 }
