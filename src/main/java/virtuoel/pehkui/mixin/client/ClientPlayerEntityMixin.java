7264af967710f10ffd514cf2c08465004e2df042
Virtuoel <Virtuoel@users.noreply.github.com>
2022-02-23 01:57:10 +0300

    1.18.2-pre2

--- src/main/java/virtuoel/pehkui/mixin/client/ClientPlayerEntityMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/client/ClientPlayerEntityMixin.java
@@ -11,14 +11,6 @@ import virtuoel.pehkui.util.ScaleUtils;
 @Mixin(ClientPlayerEntity.class)
 public class ClientPlayerEntityMixin
 {
-	@ModifyConstant(method = "sendMovementPackets", constant = @Constant(doubleValue = 9.0E-4D))
-	private double sendMovementPacketsModifyMinVelocity(double value)
-	{
-		final float scale = ScaleUtils.getMotionScale((Entity) (Object) this);
-		
-		return scale < 1.0F ? scale * scale * value : value;
-	}
-	
 	@ModifyConstant(method = "tickMovement", constant = @Constant(floatValue = 3.0F))
 	private float tickMovementModifyFlightSpeed(float value)
 	{
