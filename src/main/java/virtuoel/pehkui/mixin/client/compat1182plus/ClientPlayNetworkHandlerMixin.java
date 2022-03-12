0af6b2394ec130dc595428f42cc9697b36cefe5b
Virtuoel <Virtuoel@users.noreply.github.com>
2022-03-12 18:58:49 +0300

    Fix mixins

--- src/main/java/virtuoel/pehkui/mixin/client/compat1182plus/ClientPlayNetworkHandlerMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/client/compat1182plus/ClientPlayNetworkHandlerMixin.java
@@ -9,7 +9,7 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.network.packet.s2c.play.PlayerRespawnS2CPacket;
-// import net.minecraft.util.registry.RegistryEntry;
+import net.minecraft.util.registry.RegistryEntry;
 import net.minecraft.util.registry.RegistryKey;
 import net.minecraft.world.World;
 import net.minecraft.world.dimension.DimensionType;
@@ -19,7 +19,7 @@ import virtuoel.pehkui.util.ScaleUtils;
 public class ClientPlayNetworkHandlerMixin
 {
 	@Inject(method = "onPlayerRespawn(Lnet/minecraft/network/packet/s2c/play/PlayerRespawnS2CPacket;)V", locals = LocalCapture.CAPTURE_FAILHARD, at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;init()V"))
-	private void onOnPlayerRespawn(PlayerRespawnS2CPacket packet, CallbackInfo info, RegistryKey<World> dimension, /* RegistryEntry<DimensionType> dimensionType, */ ClientPlayerEntity oldPlayer, int id, String brand, ClientPlayerEntity newPlayer)
+	private void onOnPlayerRespawn(PlayerRespawnS2CPacket packet, CallbackInfo info, RegistryKey<World> dimension, RegistryEntry<DimensionType> dimensionType, ClientPlayerEntity oldPlayer, int id, String brand, ClientPlayerEntity newPlayer)
 	{
 		ScaleUtils.loadScaleOnRespawn(newPlayer, oldPlayer, packet.shouldKeepPlayerAttributes());
 	}
