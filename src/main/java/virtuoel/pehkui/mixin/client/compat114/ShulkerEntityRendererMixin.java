4960cec79a0eef3e7b034c4f3e9c99f0166f3f7e
Virtuoel <Virtuoel@users.noreply.github.com>
2022-04-12 00:23:30 +0300

    Tweak Shulker model mixin

--- src/main/java/virtuoel/pehkui/mixin/client/compat114/ShulkerEntityRendererMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/client/compat114/ShulkerEntityRendererMixin.java
@@ -8,6 +8,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.render.entity.ShulkerEntityRenderer;
 import net.minecraft.entity.mob.ShulkerEntity;
+import net.minecraft.util.math.Direction;
 import virtuoel.pehkui.util.ScaleUtils;
 
 @Mixin(ShulkerEntityRenderer.class)
@@ -16,22 +17,22 @@ public class ShulkerEntityRendererMixin
 	@Inject(at = @At("RETURN"), method = "method_4058", remap = false)
 	private void onSetupTransforms(ShulkerEntity entity, float animationProgress, float bodyYaw, float tickDelta, CallbackInfo info)
 	{
-		final float w = ScaleUtils.getModelWidthScale(entity, tickDelta);
-		final float h = ScaleUtils.getModelHeightScale(entity, tickDelta);
-		
-		switch (entity.getAttachedFace())
+		final Direction face = entity.getAttachedFace();
+		if (face != Direction.DOWN)
 		{
-			case NORTH:
-			case SOUTH:
-			case EAST:
-			case WEST:
-				GL11.glTranslated(0.0, -((1.0F - w) * 0.5F) / w, -((1.0F - h) * 0.5F) / h);
-				break;
-			case UP:
+			final float h = ScaleUtils.getModelHeightScale(entity, tickDelta);
+			if (face != Direction.UP)
+			{
+				final float w = ScaleUtils.getModelWidthScale(entity, tickDelta);
+				if (w != 1.0F || h != 1.0F)
+				{
+					GL11.glTranslated(0.0, -((1.0F - w) * 0.5F) / w, -((1.0F - h) * 0.5F) / h);
+				}
+			}
+			else if (h != 1.0F)
+			{
 				GL11.glTranslated(0.0, -(1.0F - h) / h, 0.0);
-				break;
-			default:
-				break;
+			}
 		}
 	}
 }
