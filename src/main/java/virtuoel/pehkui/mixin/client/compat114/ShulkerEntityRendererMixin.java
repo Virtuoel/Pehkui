3f4f7e77f112a2a6d2b4ebfcf748483490ca1ad6
Virtuoel <Virtuoel@users.noreply.github.com>
2022-04-11 18:11:55 +0300

    Shulker model fix for 1.14

--- src/main/java/virtuoel/pehkui/mixin/client/compat114/ShulkerEntityRendererMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/client/compat114/ShulkerEntityRendererMixin.java
@@ -1,5 +1,6 @@
 package virtuoel.pehkui.mixin.client.compat114;
 
+import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -7,6 +8,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.client.render.entity.ShulkerEntityRenderer;
 import net.minecraft.entity.mob.ShulkerEntity;
+import virtuoel.pehkui.util.ScaleUtils;
 
 @Mixin(ShulkerEntityRenderer.class)
 public class ShulkerEntityRendererMixin
@@ -14,6 +16,22 @@ public class ShulkerEntityRendererMixin
 	@Inject(at = @At("RETURN"), method = "method_4058", remap = false)
 	private void onSetupTransforms(ShulkerEntity entity, float animationProgress, float bodyYaw, float tickDelta, CallbackInfo info)
 	{
-		// TODO FIXME
+		final float w = ScaleUtils.getModelWidthScale(entity, tickDelta);
+		final float h = ScaleUtils.getModelHeightScale(entity, tickDelta);
+		
+		switch (entity.getAttachedFace())
+		{
+			case NORTH:
+			case SOUTH:
+			case EAST:
+			case WEST:
+				GL11.glTranslated(0.0, -((1.0F - w) * 0.5F) / w, -((1.0F - h) * 0.5F) / h);
+				break;
+			case UP:
+				GL11.glTranslated(0.0, -(1.0F - h) / h, 0.0);
+				break;
+			default:
+				break;
+		}
 	}
 }
