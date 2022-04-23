6097f668e031d5959dc6fdab616d2ea871db89b1
Virtuoel <Virtuoel@users.noreply.github.com>
2022-04-23 19:22:25 +0300

    Organize imports

--- src/main/java/virtuoel/pehkui/mixin/reach/compat119plus/StorageMinecartEntityMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/reach/compat119plus/StorageMinecartEntityMixin.java
@@ -4,6 +4,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.vehicle.StorageMinecartEntity;
 import net.minecraft.entity.vehicle.VehicleInventory;
