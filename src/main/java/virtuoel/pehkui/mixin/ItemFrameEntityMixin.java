bc5043e4e3af37e9ae7cf2fc8afc1f8f7d658397
Virtuoel <Virtuoel@users.noreply.github.com>
2022-05-27 02:35:54 +0300

    Improve Item Frame scaling
    Fixes #279

---
+++ src/main/java/virtuoel/pehkui/mixin/ItemFrameEntityMixin.java
@@ -0,0 +1,40 @@
+package virtuoel.pehkui.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import net.minecraft.entity.decoration.ItemFrameEntity;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
+import virtuoel.pehkui.util.ScaleUtils;
+
+@Mixin(ItemFrameEntity.class)
+public abstract class ItemFrameEntityMixin
+{
+	@ModifyArg(method = "updateAttachmentPosition()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/decoration/ItemFrameEntity;setBoundingBox(Lnet/minecraft/util/math/Box;)V"))
+	private Box onUpdateAttachmentPositionModifyBox(Box box)
+	{
+		final ItemFrameEntity entity = (ItemFrameEntity) (Object) this;
+		
+		final Direction facing = entity.getHorizontalFacing();
+		
+		final double xLength = box.getXLength() / -2.0D;
+		final double yLength = box.getYLength() / -2.0D;
+		final double zLength = box.getZLength() / -2.0D;
+		
+		final float widthScale = ScaleUtils.getBoundingBoxWidthScale(entity);
+		final float heightScale = ScaleUtils.getBoundingBoxHeightScale(entity);
+		
+		if (widthScale != 1.0F || heightScale != 1.0F)
+		{
+			final double dX = xLength * (1.0D - widthScale); 
+			final double dY = yLength * (1.0D - heightScale); 
+			final double dZ = zLength * (1.0D - widthScale); 
+			box = box.expand(dX, dY, dZ);
+			box = box.offset(dX * facing.getOffsetX(), dY * facing.getOffsetY(), dZ * facing.getOffsetZ());
+		}
+		
+		return box;
+	}
+}
--- src/main/java/virtuoel/pehkui/mixin/client/compat115plus/ItemFrameEntityRendererMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/client/compat115plus/ItemFrameEntityRendererMixin.java
@@ -2,19 +2,37 @@ package virtuoel.pehkui.mixin.client.compat115plus;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
+import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.entity.ItemFrameEntityRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.decoration.ItemFrameEntity;
+import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Vec3d;
+import virtuoel.pehkui.util.ScaleUtils;
 
 @Mixin(ItemFrameEntityRenderer.class)
 public class ItemFrameEntityRendererMixin
 {
-	@Inject(at = @At("RETURN"), method = "getPositionOffset", cancellable = true)
-	private void onGetPositionOffset(ItemFrameEntity entity, float tickDelta, CallbackInfoReturnable<Vec3d> info)
+	@ModifyVariable(method = "render", at = @At(value = "STORE"))
+	private Vec3d onRender(Vec3d value, ItemFrameEntity itemFrameEntity, float f, float g, MatrixStack matrixStack, VertexConsumerProvider vertexConsumerProvider, int i)
 	{
-		info.setReturnValue(info.getReturnValue().multiply(-5.0D / 48.0D, ((double) entity.getHorizontalFacing().getOffsetY()) * 0.125D, -5.0D / 48.0D));
+		final float widthScale = ScaleUtils.getBoundingBoxWidthScale(itemFrameEntity);
+		final float heightScale = ScaleUtils.getBoundingBoxHeightScale(itemFrameEntity);
+		
+		if (widthScale != 1.0F || heightScale != 1.0F)
+		{
+			value = value.multiply(1.0F / widthScale, 1.0F / heightScale, 1.0F / widthScale);
+			
+			final Direction facing = itemFrameEntity.getHorizontalFacing();
+			
+			final double widthOffset = ((0.0625D - (0.03125D * widthScale)) - 0.03125D) / widthScale;
+			final double heightOffset = ((0.0625D - (0.03125D * heightScale)) - 0.03125D) / heightScale;
+			
+			value = value.add(widthOffset * facing.getOffsetX(), heightOffset * facing.getOffsetY(), widthOffset * facing.getOffsetZ());
+		}
+		
+		return value;
 	}
 }
