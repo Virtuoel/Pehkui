1e428b550de5117ceb7ef8c1fd7c6c5d2f916dad
Virtuoel <Virtuoel@users.noreply.github.com>
2022-06-20 02:26:29 +0300

    Clean up version utils for easier expansion in the future

--- src/main/java/virtuoel/pehkui/util/VersionUtils.java
+++ src/main/java/virtuoel/pehkui/util/VersionUtils.java
@@ -1,5 +1,9 @@
 package virtuoel.pehkui.util;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
 import org.jetbrains.annotations.Nullable;
 
 import net.fabricmc.loader.api.FabricLoader;
@@ -8,92 +12,56 @@ import net.fabricmc.loader.api.Version;
 
 public class VersionUtils
 {
+	private static final List<Predicate<String>> VERSION_PREDICATES = new ArrayList<>();
+	
+	static
+	{
+		final int[][] ranges = {{1, 14, 4}, {1, 15, 2}, {1, 16, 5}, {1, 17, 1}, {1, 18, 2}, {1, 19, 1}};
+		final String prefix = ".compat%s%s";
+		
+		for (final int[] range : ranges)
+		{
+			final int major = range[0];
+			final int minor = range[1];
+			final String predicatePrefix = String.format(prefix, major, minor);
+			final String predicateMinor = predicatePrefix + ".";
+			final String predicateMinorPlus = predicatePrefix + "plus.";
+			final String predicateMinorMinus = predicatePrefix + "minus.";
+			final String predicateMinorZero = predicatePrefix + "0.";
+			final String predicateMinorZeroMinus = predicatePrefix + "0minus.";
+			
+			VERSION_PREDICATES.add(n -> n.contains(predicateMinor) && VersionUtils.MINOR != minor);
+			VERSION_PREDICATES.add(n -> n.contains(predicateMinorPlus) && VersionUtils.MINOR < minor);
+			VERSION_PREDICATES.add(n -> n.contains(predicateMinorMinus) && VersionUtils.MINOR > minor);
+			VERSION_PREDICATES.add(n -> n.contains(predicateMinorZero) && (VersionUtils.MINOR != minor || VersionUtils.PATCH != 0));
+			VERSION_PREDICATES.add(n -> n.contains(predicateMinorZeroMinus) && (VersionUtils.MINOR > minor || (VersionUtils.MINOR == minor && VersionUtils.PATCH > 0)));
+			
+			final int maxPatch = range[1];
+			
+			for (int i = 1; i <= maxPatch; i++)
+			{
+				final int patch = i;
+				final String predicatePatch = predicatePrefix + patch + ".";
+				final String predicatePatchPlus = predicatePrefix + patch + "plus.";
+				final String predicatePatchMinus = predicatePrefix + patch + "minus.";
+				
+				VERSION_PREDICATES.add(n -> n.contains(predicatePatch) && (VersionUtils.MINOR != minor || VersionUtils.PATCH != patch));
+				VERSION_PREDICATES.add(n -> n.contains(predicatePatchPlus) && (VersionUtils.MINOR < minor || (VersionUtils.MINOR == minor && VersionUtils.PATCH < patch)));
+				VERSION_PREDICATES.add(n -> n.contains(predicatePatchMinus) && (VersionUtils.MINOR > minor || (VersionUtils.MINOR == minor && VersionUtils.PATCH > patch)));
+			}
+		}
+	}
+	
 	public static boolean shouldApplyCompatibilityMixin(String mixinClassName)
 	{
 		if (mixinClassName.contains(".compat"))
 		{
-			if (
-				(mixinClassName.contains(".compat114.") && MINOR != 14) ||
-				(mixinClassName.contains(".compat114plus.") && MINOR < 14) ||
-				(mixinClassName.contains(".compat114minus.") && MINOR > 14) ||
-				(mixinClassName.contains(".compat1140.") && (MINOR != 14 || PATCH != 0)) ||
-				(mixinClassName.contains(".compat1140minus.") && (MINOR > 14 || (MINOR == 14 && PATCH > 0))) ||
-				(mixinClassName.contains(".compat1141.") && (MINOR != 14 || PATCH != 1)) ||
-				(mixinClassName.contains(".compat1141plus.") && (MINOR < 14 || (MINOR == 14 && PATCH < 1))) ||
-				(mixinClassName.contains(".compat1141minus.") && (MINOR > 14 || (MINOR == 14 && PATCH > 1))) ||
-				(mixinClassName.contains(".compat1142.") && (MINOR != 14 || PATCH != 2)) ||
-				(mixinClassName.contains(".compat1142plus.") && (MINOR < 14 || (MINOR == 14 && PATCH < 2))) ||
-				(mixinClassName.contains(".compat1142minus.") && (MINOR > 14 || (MINOR == 14 && PATCH > 2))) ||
-				(mixinClassName.contains(".compat1143.") && (MINOR != 14 || PATCH != 3)) ||
-				(mixinClassName.contains(".compat1143plus.") && (MINOR < 14 || (MINOR == 14 && PATCH < 3))) ||
-				(mixinClassName.contains(".compat1143minus.") && (MINOR > 14 || (MINOR == 14 && PATCH > 3))) ||
-				(mixinClassName.contains(".compat1144.") && (MINOR != 14 || PATCH != 4)) ||
-				(mixinClassName.contains(".compat1144plus.") && (MINOR < 14 || (MINOR == 14 && PATCH < 4))) ||
-				(mixinClassName.contains(".compat1144minus.") && (MINOR > 14 || (MINOR == 14 && PATCH > 4))) ||
-				
-				(mixinClassName.contains(".compat115.") && MINOR != 15) ||
-				(mixinClassName.contains(".compat115plus.") && MINOR < 15) ||
-				(mixinClassName.contains(".compat115minus.") && MINOR > 15) ||
-				(mixinClassName.contains(".compat1150.") && (MINOR != 15 || PATCH != 0)) ||
-				(mixinClassName.contains(".compat1150minus.") && (MINOR > 15 || (MINOR == 15 && PATCH > 0))) ||
-				(mixinClassName.contains(".compat1151.") && (MINOR != 15 || PATCH != 1)) ||
-				(mixinClassName.contains(".compat1151plus.") && (MINOR < 15 || (MINOR == 15 && PATCH < 1))) ||
-				(mixinClassName.contains(".compat1151minus.") && (MINOR > 15 || (MINOR == 15 && PATCH > 1))) ||
-				(mixinClassName.contains(".compat1152.") && (MINOR != 15 || PATCH != 2)) ||
-				(mixinClassName.contains(".compat1152plus.") && (MINOR < 15 || (MINOR == 15 && PATCH < 2))) ||
-				(mixinClassName.contains(".compat1152minus.") && (MINOR > 15 || (MINOR == 15 && PATCH > 2))) ||
-				
-				(mixinClassName.contains(".compat116.") && MINOR != 16) ||
-				(mixinClassName.contains(".compat116plus.") && MINOR < 16) ||
-				(mixinClassName.contains(".compat116minus.") && MINOR > 16) ||
-				(mixinClassName.contains(".compat1160.") && (MINOR != 16 || PATCH != 0)) ||
-				(mixinClassName.contains(".compat1160minus.") && (MINOR > 16 || (MINOR == 16 && PATCH > 0))) ||
-				(mixinClassName.contains(".compat1161.") && (MINOR != 16 || PATCH != 1)) ||
-				(mixinClassName.contains(".compat1161plus.") && (MINOR < 16 || (MINOR == 16 && PATCH < 1))) ||
-				(mixinClassName.contains(".compat1161minus.") && (MINOR > 16 || (MINOR == 16 && PATCH > 1))) ||
-				(mixinClassName.contains(".compat1162.") && (MINOR != 16 || PATCH != 2)) ||
-				(mixinClassName.contains(".compat1162plus.") && (MINOR < 16 || (MINOR == 16 && PATCH < 2))) ||
-				(mixinClassName.contains(".compat1162minus.") && (MINOR > 16 || (MINOR == 16 && PATCH > 2))) ||
-				(mixinClassName.contains(".compat1163.") && (MINOR != 16 || PATCH != 3)) ||
-				(mixinClassName.contains(".compat1163plus.") && (MINOR < 16 || (MINOR == 16 && PATCH < 3))) ||
-				(mixinClassName.contains(".compat1163minus.") && (MINOR > 16 || (MINOR == 16 && PATCH > 3))) ||
-				(mixinClassName.contains(".compat1164.") && (MINOR != 16 || PATCH != 4)) ||
-				(mixinClassName.contains(".compat1164plus.") && (MINOR < 16 || (MINOR == 16 && PATCH < 4))) ||
-				(mixinClassName.contains(".compat1164minus.") && (MINOR > 16 || (MINOR == 16 && PATCH > 4))) ||
-				(mixinClassName.contains(".compat1165.") && (MINOR != 16 || PATCH != 5)) ||
-				(mixinClassName.contains(".compat1165plus.") && (MINOR < 16 || (MINOR == 16 && PATCH < 5))) ||
-				(mixinClassName.contains(".compat1165minus.") && (MINOR > 16 || (MINOR == 16 && PATCH > 5))) ||
-				
-				(mixinClassName.contains(".compat117.") && MINOR != 17) ||
-				(mixinClassName.contains(".compat117plus.") && MINOR < 17) ||
-				(mixinClassName.contains(".compat117minus.") && MINOR > 17) ||
-				(mixinClassName.contains(".compat1170.") && (MINOR != 17 || PATCH != 0)) ||
-				(mixinClassName.contains(".compat1170minus.") && (MINOR > 17 || (MINOR == 17 && PATCH > 0))) ||
-				(mixinClassName.contains(".compat1171.") && (MINOR != 17 || PATCH != 1)) ||
-				(mixinClassName.contains(".compat1171plus.") && (MINOR < 17 || (MINOR == 17 && PATCH < 1))) ||
-				(mixinClassName.contains(".compat1171minus.") && (MINOR > 17 || (MINOR == 17 && PATCH > 1))) ||
-				
-				(mixinClassName.contains(".compat118.") && MINOR != 18) ||
-				(mixinClassName.contains(".compat118plus.") && MINOR < 18) ||
-				(mixinClassName.contains(".compat118minus.") && MINOR > 18) ||
-				(mixinClassName.contains(".compat1180.") && (MINOR != 18 || PATCH != 0)) ||
-				(mixinClassName.contains(".compat1180minus.") && (MINOR > 18 || (MINOR == 18 && PATCH > 0))) ||
-				(mixinClassName.contains(".compat1181.") && (MINOR != 18 || PATCH != 1)) ||
-				(mixinClassName.contains(".compat1181plus.") && (MINOR < 18 || (MINOR == 18 && PATCH < 1))) ||
-				(mixinClassName.contains(".compat1181minus.") && (MINOR > 18 || (MINOR == 18 && PATCH > 1))) ||
-				(mixinClassName.contains(".compat1182.") && (MINOR != 18 || PATCH != 2)) ||
-				(mixinClassName.contains(".compat1182plus.") && (MINOR < 18 || (MINOR == 18 && PATCH < 2))) ||
-				(mixinClassName.contains(".compat1182minus.") && (MINOR > 18 || (MINOR == 18 && PATCH > 2))) ||
-				
-				(mixinClassName.contains(".compat119.") && MINOR != 19) ||
-				(mixinClassName.contains(".compat119plus.") && MINOR < 19) ||
-				(mixinClassName.contains(".compat119minus.") && MINOR > 19) ||
-				(mixinClassName.contains(".compat1190.") && (MINOR != 19 || PATCH != 0)) ||
-				(mixinClassName.contains(".compat1190minus.") && (MINOR > 19 || (MINOR == 19 && PATCH > 0))) ||
-				false
-			)
+			for (final Predicate<String> predicate : VERSION_PREDICATES)
 			{
-				return false;
+				if (predicate.test(mixinClassName))
+				{
+					return false;
+				}
 			}
 		}
 		
