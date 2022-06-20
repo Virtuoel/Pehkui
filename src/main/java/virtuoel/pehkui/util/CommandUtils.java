950cad0cb708da371cbc3fb119ce8ef64bbeb4eb
Virtuoel <Virtuoel@users.noreply.github.com>
2022-06-20 05:18:47 +0300

    Refactor to calls placed similarly to those of previous version

--- src/main/java/virtuoel/pehkui/util/CommandUtils.java
+++ src/main/java/virtuoel/pehkui/util/CommandUtils.java
@@ -4,8 +4,10 @@ import java.util.Locale;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 import com.mojang.brigadier.CommandDispatcher;
+import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 
@@ -16,7 +18,6 @@ import net.minecraft.predicate.NumberRange;
 import net.minecraft.server.command.ServerCommandSource;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import virtuoel.pehkui.Pehkui;
@@ -28,35 +29,13 @@ import virtuoel.pehkui.server.command.ScaleCommand;
 
 public class CommandUtils
 {
+	private static final DeferredRegister<ArgumentSerializer<?, ?>> COMMAND_ARGUMENT_TYPES = DeferredRegister.create(Registry.COMMAND_ARGUMENT_TYPE_KEY, Pehkui.MOD_ID);
+	
 	public static void registerArgumentTypes()
 	{
-		final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		registerArgumentTypes(CommandUtils::registerConstantArgumentType);
 		
-		final DeferredRegister<ArgumentSerializer<?, ?>> registry = DeferredRegister.create(Registry.COMMAND_ARGUMENT_TYPE_KEY, Pehkui.MOD_ID);
-		
-		registry.register(
-			"scale_type",
-			() -> ArgumentTypes.registerByClass(
-					ScaleTypeArgumentType.class,
-					ConstantArgumentSerializer.of(ScaleTypeArgumentType::scaleType)
-				)
-		);
-		registry.register(
-			"scale_modifier",
-			() -> ArgumentTypes.registerByClass(
-					ScaleModifierArgumentType.class,
-					ConstantArgumentSerializer.of(ScaleModifierArgumentType::scaleModifier)
-				)
-		);
-		registry.register(
-			"scale_operation",
-			() -> ArgumentTypes.registerByClass(
-					ScaleOperationArgumentType.class,
-					ConstantArgumentSerializer.of(ScaleOperationArgumentType::operation)
-				)
-		);
-		
-		registry.register(modEventBus);
+		COMMAND_ARGUMENT_TYPES.register(FMLJavaModLoadingContext.get().getModEventBus());
 	}
 	
 	public static void registerCommands(final CommandDispatcher<ServerCommandSource> dispatcher)
@@ -65,9 +44,33 @@ public class CommandUtils
 		DebugCommand.register(dispatcher);
 	}
 	
+	public static void registerArgumentTypes(ArgumentTypeConsumer consumer)
+	{
+		consumer.register(Pehkui.id("scale_type"), ScaleTypeArgumentType.class, ScaleTypeArgumentType::scaleType);
+		consumer.register(Pehkui.id("scale_modifier"), ScaleModifierArgumentType.class, ScaleModifierArgumentType::scaleModifier);
+		consumer.register(Pehkui.id("scale_operation"), ScaleOperationArgumentType.class, ScaleOperationArgumentType::operation);
+	}
+	
+	@FunctionalInterface
+	public interface ArgumentTypeConsumer
+	{
+		<T extends ArgumentType<?>> void register(Identifier id, Class<T> argClass, Supplier<T> supplier);
+	}
+	
 	public static boolean testFloatRange(NumberRange.FloatRange range, float value)
 	{
 		return range.test(value);
+	}
+	
+	public static <T extends ArgumentType<?>> void registerConstantArgumentType(Identifier id, Class<T> argClass, Supplier<T> supplier)
+	{
+		COMMAND_ARGUMENT_TYPES.register(
+			id.getPath(),
+			() -> ArgumentTypes.registerByClass(
+					argClass,
+					ConstantArgumentSerializer.of(supplier)
+				)
+		);
 	}
 	
 	public static CompletableFuture<Suggestions> suggestIdentifiersIgnoringNamespace(String namespace, Iterable<Identifier> candidates, SuggestionsBuilder builder)
