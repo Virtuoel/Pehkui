54ca78c570cf483f6b671d5cc02126aa6548a12c
Virtuoel <Virtuoel@users.noreply.github.com>
2022-07-01 00:43:16 +0300

    Fix crash on servers in 1.15.2 and below

--- src/main/java/virtuoel/pehkui/mixin/client/compat115minus/OtherClientPlayerEntityMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/client/compat115minus/OtherClientPlayerEntityMixin.java
@@ -1,4 +1,4 @@
-package virtuoel.pehkui.mixin.compat115minus;
+package virtuoel.pehkui.mixin.client.compat115minus;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.Constant;
