821432818b3832f18fb84c425361d10fcbdaee62
Virtuoel <Virtuoel@users.noreply.github.com>
2022-07-18 01:15:44 +0300

    Refactor easings
    Refactored to use public domain code. Original easings looked rather
    similar to easing snippets from a GPL-3.0 site where the licensing of
    the snippets themselves was not explicitly specified

--- src/main/java/virtuoel/pehkui/api/ScaleEasings.java
+++ src/main/java/virtuoel/pehkui/api/ScaleEasings.java
@@ -6,82 +6,219 @@ import virtuoel.pehkui.Pehkui;
 
 public final class ScaleEasings
 {
-	private static final float c1 = 1.70158F;
-	private static final float c2 = c1 * 1.525F;
-	private static final float c3 = c1 + 1;
-	private static final float c4 = (float) ((2 * Math.PI) / 3);
-	private static final float c5 = (float) ((2 * Math.PI) / 4.5);
-	private static final float n1 = 7.5625F;
-	private static final float d1 = 2.75F;
+	private static final double HALF_PI = Math.PI / 2.0D;
 	
 	public static final FloatUnaryOperator LINEAR = register("linear", FloatUnaryOperator.identity());
 	
-	public static final FloatUnaryOperator SINE_IN = register("sine_in", x -> (float) (1 - Math.cos((x * Math.PI) / 2)));
-	public static final FloatUnaryOperator SINE_OUT = register("sine_out", x -> (float) Math.sin((x * Math.PI) / 2));
-	public static final FloatUnaryOperator SINE_IN_OUT = register("sine_in_out", x -> (float) -(Math.cos(Math.PI * x) - 1) / 2);
-	
-	public static final FloatUnaryOperator QUAD_IN = register("quad_in", x -> x * x);
-	public static final FloatUnaryOperator QUAD_OUT = register("quad_out", x -> 1 - (1 - x) * (1 - x));
-	public static final FloatUnaryOperator QUAD_IN_OUT = register("quad_in_out", x -> x < 0.5 ? 2 * x * x : (float) (1 - Math.pow(-2 * x + 2, 2) / 2));
-	
-	public static final FloatUnaryOperator CUBIC_IN = register("cubic_in", x -> x * x * x);
-	public static final FloatUnaryOperator CUBIC_OUT = register("cubic_out", x -> (float) (1 - Math.pow(1 - x, 3)));
-	public static final FloatUnaryOperator CUBIC_IN_OUT = register("cubic_in_out", x -> x < 0.5 ? 4 * x * x * x : (float) (1 - Math.pow(-2 * x + 2, 3) / 2));
-	
-	public static final FloatUnaryOperator QUART_IN = register("quart_in", x -> x * x * x * x);
-	public static final FloatUnaryOperator QUART_OUT = register("quart_out", x -> (float) (1 - Math.pow(1 - x, 4)));
-	public static final FloatUnaryOperator QUART_IN_OUT = register("quart_in_out", x -> x < 0.5 ? 8 * x * x * x * x : (float) (1 - Math.pow(-2 * x + 2, 4) / 2));
-	
-	public static final FloatUnaryOperator QUINT_IN = register("quint_in", x -> x * x * x * x * x);
-	public static final FloatUnaryOperator QUINT_OUT = register("quint_out", x -> (float) (1 - Math.pow(1 - x, 5)));
-	public static final FloatUnaryOperator QUINT_IN_OUT = register("quint_in_out", x -> x < 0.5 ? 16 * x * x * x * x * x : (float) (1 - Math.pow(-2 * x + 2, 5) / 2));
-	
-	public static final FloatUnaryOperator EXPO_IN = register("expo_in", x -> x == 0 ? 0 : (float) Math.pow(2, 10 * x - 10));
-	public static final FloatUnaryOperator EXPO_OUT = register("expo_out", x -> x == 1 ? 1 : (float) (1 - Math.pow(2, -10 * x)));
-	public static final FloatUnaryOperator EXPO_IN_OUT = register("expo_in_out", x -> x == 0 ? 0 : (float) (x == 1 ? 1 : x < 0.5 ? Math.pow(2, 20 * x - 10) / 2 : (2 - Math.pow(2, -20 * x + 10)) / 2));
-	
-	public static final FloatUnaryOperator CIRC_IN = register("circ_in", x -> (float) (1 - Math.sqrt(1 - Math.pow(x, 2))));
-	public static final FloatUnaryOperator CIRC_OUT = register("circ_out", x -> (float) Math.sqrt(1 - Math.pow(x - 1, 2)));
-	public static final FloatUnaryOperator CIRC_IN_OUT = register("circ_in_out", x -> (float) (x < 0.5 ? (1 - Math.sqrt(1 - Math.pow(2 * x, 2))) / 2 : (Math.sqrt(1 - Math.pow(-2 * x + 2, 2)) + 1) / 2));
-	
-	public static final FloatUnaryOperator BACK_IN = register("back_in", x ->
+	public static final FloatUnaryOperator QUADRATIC_IN = register("quadratic_in", x -> x * x);
+	public static final FloatUnaryOperator QUADRATIC_OUT = register("quadratic_out", x ->
 	{
-		return c3 * x * x * x - c1 * x * x;
+		return -(x * (x - 2.0F));
 	});
-	public static final FloatUnaryOperator BACK_OUT = register("back_out", x ->
+	public static final FloatUnaryOperator QUADRATIC_IN_OUT = register("quadratic_in_out", x ->
 	{
-		return (float) (1 + c3 * Math.pow(x - 1, 3) + c1 * Math.pow(x - 1, 2));
-	});
-	public static final FloatUnaryOperator BACK_IN_OUT = register("back_in_out", x ->
-	{
-		return (float) (x < 0.5 ? (Math.pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2 : (Math.pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2);
-	});
-	
-	public static final FloatUnaryOperator ELASTIC_IN = register("elastic_in", x -> x == 0 ? 0 : (float) (x == 1 ? 1 : -Math.pow(2, 10 * x - 10) * Math.sin((x * 10 - 10.75) * c4)));
-	public static final FloatUnaryOperator ELASTIC_OUT = register("elastic_out", x -> x == 0 ? 0 : (float) (x == 1 ? 1 : Math.pow(2, -10 * x) * Math.sin((x * 10 - 0.75) * c4) + 1));
-	public static final FloatUnaryOperator ELASTIC_IN_OUT = register("elastic_in_out", x -> x == 0 ? 0 : (float) (x == 1 ? 1 : x < 0.5 ? -(Math.pow(2, 20 * x - 10) * Math.sin((20 * x - 11.125) * c5)) / 2 : (Math.pow(2, -20 * x + 10) * Math.sin((20 * x - 11.125) * c5)) / 2 + 1));
-	
-	public static final FloatUnaryOperator BOUNCE_IN = register("bounce_in", x -> 1 - ScaleEasings.BOUNCE_OUT.apply(1 - x));
-	public static final FloatUnaryOperator BOUNCE_OUT = register("bounce_out", x ->
-	{
-		if (x < 1 / d1)
+		if (x < 0.5F)
 		{
-			return n1 * x * x;
-		}
-		else if (x < 2 / d1)
-		{
-			return n1 * (x -= 1.5 / d1) * x + 0.75F;
-		}
-		else if (x < 2.5 / d1)
-		{
-			return n1 * (x -= 2.25 / d1) * x + 0.9375F;
+			return 2.0F * x * x;
 		}
 		else
 		{
-			return n1 * (x -= 2.625 / d1) * x + 0.984375F;
+			return (-2.0F * x * x) + (4.0F * x) - 1.0F;
 		}
 	});
-	public static final FloatUnaryOperator BOUNCE_IN_OUT = register("bounce_in_out", x -> x < 0.5 ? (1 - ScaleEasings.BOUNCE_OUT.apply(1 - 2 * x)) / 2 : (1 + ScaleEasings.BOUNCE_OUT.apply(2 * x - 1)) / 2);
+	
+	public static final FloatUnaryOperator CUBIC_IN = register("cubic_in", x -> x * x * x);
+	public static final FloatUnaryOperator CUBIC_OUT = register("cubic_out", x ->
+	{
+		float f = x - 1.0F;
+		return f * f * f + 1.0F;
+	});
+	public static final FloatUnaryOperator CUBIC_IN_OUT = register("cubic_in_out", x ->
+	{
+		if (x < 0.5F)
+		{
+			return 4.0F * x * x * x;
+		}
+		else
+		{
+			float f = (2.0F * x) - 2.0F;
+			return 0.5F * f * f * f + 1.0F;
+		}
+	});
+	
+	public static final FloatUnaryOperator QUARTIC_IN = register("quartic_in", x -> x * x * x * x);
+	public static final FloatUnaryOperator QUARTIC_OUT = register("quartic_out", x ->
+	{
+		float f = x - 1.0F;
+		return f * f * f * (1.0F - x) + 1.0F;
+	});
+	public static final FloatUnaryOperator QUARTIC_IN_OUT = register("quartic_in_out", x ->
+	{
+		if (x < 0.5F)
+		{
+			return 8.0F * x * x * x * x;
+		}
+		else
+		{
+			float f = x - 1.0F;
+			return -8.0F * f * f * f * f + 1.0F;
+		}
+	});
+	
+	public static final FloatUnaryOperator QUINTIC_IN = register("quintic_in", x -> x * x * x * x * x);
+	public static final FloatUnaryOperator QUINTIC_OUT = register("quintic_out", x ->
+	{
+		float f = x - 1.0F;
+		return f * f * f * f * f + 1.0F;
+	});
+	public static final FloatUnaryOperator QUINTIC_IN_OUT = register("quintic_in_out", x ->
+	{
+		if (x < 0.5F)
+		{
+			return 16.0F * x * x * x * x * x;
+		}
+		else
+		{
+			float f = (2.0F * x) - 2.0F;
+			return 0.5F * f * f * f * f * f + 1.0F;
+		}
+	});
+	
+	public static final FloatUnaryOperator SINE_IN = register("sine_in", x ->
+	{
+		return (float) Math.sin((x - 1.0F) * HALF_PI) + 1.0F;
+	});
+	public static final FloatUnaryOperator SINE_OUT = register("sine_out", x ->
+	{
+		return (float) Math.sin(x * HALF_PI);
+	});
+	public static final FloatUnaryOperator SINE_IN_OUT = register("sine_in_out", x ->
+	{
+		return (float) (0.5F * (1.0F - Math.cos(x * Math.PI)));
+	});
+	
+	public static final FloatUnaryOperator CIRCULAR_IN = register("circular_in", x ->
+	{
+		return (float) (1.0F - Math.sqrt(1.0F - (x * x)));
+	});
+	public static final FloatUnaryOperator CIRCULAR_OUT = register("circular_out", x ->
+	{
+		return (float) Math.sqrt((2.0F - x) * x);
+	});
+	public static final FloatUnaryOperator CIRCULAR_IN_OUT = register("circular_in_out", x ->
+	{
+		if (x < 0.5F)
+		{
+			return (float) (0.5F * (1.0F - Math.sqrt(1.0F - (4.0F * x * x))));
+		}
+		else
+		{
+			return (float) (0.5F * (Math.sqrt(-((2.0F * x) - 3.0F) * ((2.0F * x) - 1.0F)) + 1.0F));
+		}
+	});
+	
+	public static final FloatUnaryOperator EXPONENTIAL_IN = register("exponential_in", x ->
+	{
+		return (float) (x == 0.0F ? x : Math.pow(2.0F, 10.0F * (x - 1.0F)));
+	});
+	public static final FloatUnaryOperator EXPONENTIAL_OUT = register("exponential_out", x ->
+	{
+		return (float) (x == 1.0F ? x : 1.0F - Math.pow(2.0F, -10.0F * x));
+	});
+	public static final FloatUnaryOperator EXPONENTIAL_IN_OUT = register("exponential_in_out", x ->
+	{
+		if (x == 0.0F || x == 1.0F)
+		{
+			return x;
+		}
+		
+		if (x < 0.0F)
+		{
+			return (float) (0.5F * Math.pow(2.0F, (20.0F * x) - 10.0F));
+		}
+		else
+		{
+			return (float) (-0.5F * Math.pow(2.0F, (-20.0F * x) + 10.0F) + 1.0F);
+		}
+	});
+	
+	public static final FloatUnaryOperator ELASTIC_IN = register("elastic_in", x ->
+	{
+		return (float) (Math.sin(13.0F * HALF_PI * x) * Math.pow(2.0F, 10.0F * (x - 1.0F)));
+	});
+	public static final FloatUnaryOperator ELASTIC_OUT = register("elastic_out", x ->
+	{
+		return (float) (Math.sin(-13.0F * HALF_PI * (x + 1.0F)) * Math.pow(2.0F, -10.0F * x) + 1.0F);
+	});
+	public static final FloatUnaryOperator ELASTIC_IN_OUT = register("elastic_in_out", x ->
+	{
+		if (x < 0.5F)
+		{
+			return (float) (0.5F * Math.sin(13.0F * HALF_PI * (2.0F * x)) * Math.pow(2.0F, 10.0F * ((2.0F * x) - 1.0F)));
+		}
+		else
+		{
+			return (float) (0.5F * (Math.sin(-13.0F * HALF_PI * ((2.0F * x - 1.0F) + 1.0F)) * Math.pow(2.0F, -10.0F * (2.0F * x - 1.0F)) + 2.0F));
+		}
+	});
+	
+	public static final FloatUnaryOperator BACK_IN = register("back_in", x ->
+	{
+		return (float) (x * x * x - x * Math.sin(x * Math.PI));
+	});
+	public static final FloatUnaryOperator BACK_OUT = register("back_out", x ->
+	{
+		float f = 1.0F - x;
+		return (float) (1.0F - (f * f * f - f * Math.sin(f * Math.PI)));
+	});
+	public static final FloatUnaryOperator BACK_IN_OUT = register("back_in_out", x ->
+	{
+		if (x < 0.5F)
+		{
+			float f = 2.0F * x;
+			return (float) (0.5F * (f * f * f - f * Math.sin(f * Math.PI)));
+		}
+		else
+		{
+			float f = 1.0F - (2.0F * x - 1.0F);
+			return (float) (0.5F * (1.0F - (f * f * f - f * Math.sin(f * Math.PI))) + 0.5F);
+		}
+	});
+	
+	public static final FloatUnaryOperator BOUNCE_IN = register("bounce_in", x ->
+	{
+		return 1.0F - ScaleEasings.BOUNCE_OUT.apply(1.0F - x);
+	});
+	public static final FloatUnaryOperator BOUNCE_OUT = register("bounce_out", x ->
+	{
+		if (x < 4.0F / 11.0F)
+		{
+			return (121.0F * x * x) / 16.0F;
+		}
+		else if (x < 8.0F / 11.0F)
+		{
+			return (363.0F / 40.0F * x * x) - (99.0F / 10.0F * x) + 17.0F / 5.0F;
+		}
+		else if (x < 9 / 10.0)
+		{
+			return (4356.0F / 361.0F * x * x) - (35442.0F / 1805.0F * x) + 16061.0F / 1805.0F;
+		}
+		else
+		{
+			return (54.0F / 5.0F * x * x) - (513.0F / 25.0F * x) + 268.0F / 25.0F;
+		}
+	});
+	public static final FloatUnaryOperator BOUNCE_IN_OUT = register("bounce_in_out", x ->
+	{
+		if (x < 0.5F)
+		{
+			return 0.5F * ScaleEasings.BOUNCE_IN.apply(x * 2.0F);
+		}
+		else
+		{
+			return 0.5F * ScaleEasings.BOUNCE_OUT.apply(x * 2.0F - 1.0F) + 0.5F;
+		}
+	});
 	
 	private static FloatUnaryOperator register(Identifier id, FloatUnaryOperator easing)
 	{
