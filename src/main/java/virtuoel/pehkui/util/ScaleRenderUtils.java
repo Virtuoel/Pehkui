ab2cb0653dda2f9a08ab7b8d4892c75fd0cc488f
Virtuoel <Virtuoel@users.noreply.github.com>
2022-08-07 23:18:45 +0300

    Deduplicate item renderer debug logging

--- src/main/java/virtuoel/pehkui/util/ScaleRenderUtils.java
+++ src/main/java/virtuoel/pehkui/util/ScaleRenderUtils.java
@@ -1,8 +1,14 @@
 package virtuoel.pehkui.util;
 
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
 import org.jetbrains.annotations.Nullable;
 
 import net.minecraft.entity.Entity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import virtuoel.pehkui.Pehkui;
 import virtuoel.pehkui.api.PehkuiConfig;
 
 public class ScaleRenderUtils
@@ -33,5 +39,41 @@ public class ScaleRenderUtils
 		}
 		
 		return depth;
+	}
+	
+	private static final Set<Item> loggedItems = ConcurrentHashMap.newKeySet();
+	private static ItemStack lastRenderedStack = null;
+	
+	public static void logIfCancelledRender()
+	{
+		if (lastRenderedStack != null)
+		{
+			final Item i = lastRenderedStack.getItem();
+			if (!loggedItems.contains(i))
+			{
+				final String stackKey = lastRenderedStack.getTranslationKey();
+				final String itemKey = lastRenderedStack.getItem().getTranslationKey();
+				if (stackKey.equals(itemKey))
+				{
+					Pehkui.LOGGER.fatal("[{}]: Was item rendering cancelled? Matrix stack not popped after rendering item {} ({}).", Pehkui.MOD_ID, stackKey, lastRenderedStack.getItem());
+				}
+				else
+				{
+					Pehkui.LOGGER.fatal("[{}]: Was item rendering cancelled? Matrix stack not popped after rendering item {} ({}) ({})", Pehkui.MOD_ID, stackKey, itemKey, lastRenderedStack.getItem());
+				}
+				
+				loggedItems.add(i);
+			}
+		}
+	}
+	
+	public static void saveLastRenderedItem(final ItemStack currentStack)
+	{
+		lastRenderedStack = currentStack;
+	}
+	
+	public static void clearLastRenderedItem()
+	{
+		lastRenderedStack = null;
 	}
 }
