cee1f0d74b8304b887f7c27962e5e31a34de3181
Virtuoel <Virtuoel@users.noreply.github.com>
2022-08-30 02:04:27 +0300

    Fix reach not working for low scales in 1.19.x

--- src/main/java/virtuoel/pehkui/mixin/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/ServerPlayNetworkHandlerMixin.java
@@ -3,9 +3,12 @@ package virtuoel.pehkui.mixin;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 import net.minecraft.entity.MovementType;
+import net.minecraft.network.packet.c2s.play.PlayerInteractBlockC2SPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.math.Vec3d;
@@ -35,5 +38,23 @@ public class ServerPlayNetworkHandlerMixin
 	{
 		final float scale = ScaleUtils.getMotionScale(player);
 		return scale != 1.0F ? movement.multiply(1.0F / scale) : movement;
+	}
+	
+	@ModifyConstant(method = "onPlayerInteractBlock", constant = @Constant(doubleValue = 0.5D, ordinal = 0))
+	private double pehkui$onPlayerInteractBlock$xOffset(double value, PlayerInteractBlockC2SPacket packet)
+	{
+		return ScaleUtils.getBlockXOffset(packet.getBlockHitResult().getBlockPos(), player);
+	}
+	
+	@ModifyConstant(method = "onPlayerInteractBlock", constant = @Constant(doubleValue = 0.5D, ordinal = 1))
+	private double pehkui$onPlayerInteractBlock$yOffset(double value, PlayerInteractBlockC2SPacket packet)
+	{
+		return ScaleUtils.getBlockYOffset(packet.getBlockHitResult().getBlockPos(), player);
+	}
+	
+	@ModifyConstant(method = "onPlayerInteractBlock", constant = @Constant(doubleValue = 0.5D, ordinal = 2))
+	private double pehkui$onPlayerInteractBlock$zOffset(double value, PlayerInteractBlockC2SPacket packet)
+	{
+		return ScaleUtils.getBlockZOffset(packet.getBlockHitResult().getBlockPos(), player);
 	}
 }
