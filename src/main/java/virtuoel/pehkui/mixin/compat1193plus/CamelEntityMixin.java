37aad74925c52f4c600d3bf0c47fcdb26eb6e9a9
Virtuoel <Virtuoel@users.noreply.github.com>
2022-11-08 00:16:16 +0300

    Fix sitting camels not being scaled

---
+++ src/main/java/virtuoel/pehkui/mixin/compat1193plus/CamelEntityMixin.java
@@ -0,0 +1,25 @@
+package virtuoel.pehkui.mixin.compat1193plus;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityDimensions;
+import net.minecraft.entity.EntityPose;
+import net.minecraft.entity.passive.CamelEntity;
+import virtuoel.pehkui.util.ScaleUtils;
+
+@Mixin(CamelEntity.class)
+public class CamelEntityMixin
+{
+	@Inject(at = @At("RETURN"), method = "getDimensions", cancellable = true)
+	private void pehkui$getDimensions(EntityPose pose, CallbackInfoReturnable<EntityDimensions> info)
+	{
+		if (pose == EntityPose.SITTING)
+		{
+			info.setReturnValue(info.getReturnValue().scaled(ScaleUtils.getBoundingBoxWidthScale((Entity) (Object) this), ScaleUtils.getBoundingBoxHeightScale((Entity) (Object) this)));
+		}
+	}
+}
