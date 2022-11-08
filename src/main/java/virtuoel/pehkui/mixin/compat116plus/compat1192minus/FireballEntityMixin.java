91a2a01f5ad632f45c94e231b2246eb12c9d7f48
Virtuoel <Virtuoel@users.noreply.github.com>
2022-11-08 07:50:05 +0300

    Fix mixins so they work

---
+++ src/main/java/virtuoel/pehkui/mixin/compat116plus/compat1192minus/FireballEntityMixin.java
@@ -0,0 +1,26 @@
+package virtuoel.pehkui.mixin.compat116plus.compat1192minus;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.projectile.FireballEntity;
+import virtuoel.pehkui.util.ScaleUtils;
+
+@Mixin(FireballEntity.class)
+public abstract class FireballEntityMixin
+{
+	@ModifyArg(method = "onCollision(Lnet/minecraft/util/hit/HitResult;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;createExplosion(Lnet/minecraft/entity/Entity;DDDFZLnet/minecraft/world/explosion/Explosion$DestructionType;)Lnet/minecraft/world/explosion/Explosion;"))
+	private float pehkui$onCollision$createExplosion(float power)
+	{
+		final float scale = ScaleUtils.getExplosionScale((Entity) (Object) this);
+		
+		if (scale != 1.0F)
+		{
+			return power * scale;
+		}
+		
+		return power;
+	}
+}
--- src/main/java/virtuoel/pehkui/mixin/compat1193plus/FireballEntityMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/compat1193plus/FireballEntityMixin.java
@@ -1,4 +1,4 @@
-package virtuoel.pehkui.mixin.compat116plus;
+package virtuoel.pehkui.mixin.compat1193plus;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,7 +11,7 @@ import virtuoel.pehkui.util.ScaleUtils;
 @Mixin(FireballEntity.class)
 public abstract class FireballEntityMixin
 {
-	@ModifyArg(method = "onCollision(Lnet/minecraft/util/hit/HitResult;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;createExplosion(Lnet/minecraft/entity/Entity;DDDFZLnet/minecraft/world/explosion/Explosion$DestructionType;)Lnet/minecraft/world/explosion/Explosion;"))
+	@ModifyArg(method = "onCollision(Lnet/minecraft/util/hit/HitResult;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;createExplosion(Lnet/minecraft/entity/Entity;DDDFZLnet/minecraft/world/World$ExplosionSourceType;)Lnet/minecraft/world/explosion/Explosion;"))
 	private float pehkui$onCollision$createExplosion(float power)
 	{
 		final float scale = ScaleUtils.getExplosionScale((Entity) (Object) this);
