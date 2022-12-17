54c60aa472143a407da0e68a03b7f56a082c94f5
Virtuoel <Virtuoel@users.noreply.github.com>
2022-12-17 19:33:39 +0300

    Ensure non-required injections handle mixin debug correctly
    Fixes #347

--- src/main/java/virtuoel/pehkui/mixin/EntitySelectorOptionsMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/EntitySelectorOptionsMixin.java
@@ -14,13 +14,13 @@ import virtuoel.pehkui.util.PehkuiEntityExtensions;
 @Mixin(EntitySelectorOptions.class)
 public class EntitySelectorOptionsMixin
 {
-	@Inject(method = "method_9957", require = 0, at = @At(value = "INVOKE", shift = Shift.BEFORE, target = "Lnet/minecraft/entity/Entity;writeNbt(Lnet/minecraft/nbt/NbtCompound;)Lnet/minecraft/nbt/NbtCompound;"))
+	@Inject(method = "method_9957", require = 0, expect = 0, at = @At(value = "INVOKE", shift = Shift.BEFORE, target = "Lnet/minecraft/entity/Entity;writeNbt(Lnet/minecraft/nbt/NbtCompound;)Lnet/minecraft/nbt/NbtCompound;"))
 	private static void pehkui$nbtSelector$before(NbtCompound nbt, boolean negated, Entity entity, CallbackInfoReturnable<Boolean> info)
 	{
 		((PehkuiEntityExtensions) entity).pehkui_setShouldIgnoreScaleNbt(true);
 	}
 	
-	@Inject(method = "method_9957", require = 0, at = @At(value = "INVOKE", shift = Shift.AFTER, target = "Lnet/minecraft/entity/Entity;writeNbt(Lnet/minecraft/nbt/NbtCompound;)Lnet/minecraft/nbt/NbtCompound;"))
+	@Inject(method = "method_9957", require = 0, expect = 0, at = @At(value = "INVOKE", shift = Shift.AFTER, target = "Lnet/minecraft/entity/Entity;writeNbt(Lnet/minecraft/nbt/NbtCompound;)Lnet/minecraft/nbt/NbtCompound;"))
 	private static void pehkui$nbtSelector$after(NbtCompound nbt, boolean negated, Entity entity, CallbackInfoReturnable<Boolean> info)
 	{
 		((PehkuiEntityExtensions) entity).pehkui_setShouldIgnoreScaleNbt(false);
