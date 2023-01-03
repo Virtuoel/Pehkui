51eb6592285d38a042ec5658b8341aff19bed8c8
Virtuoel <Virtuoel@users.noreply.github.com>
2023-01-03 05:42:35 +0300

    Fix hitbox size in inventory

--- src/main/java/virtuoel/pehkui/mixin/client/InventoryScreenMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/client/InventoryScreenMixin.java
@@ -11,6 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import it.unimi.dsi.fastutil.objects.Object2ObjectLinkedOpenHashMap;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.Box;
 import virtuoel.pehkui.api.ScaleData;
 import virtuoel.pehkui.api.ScaleRegistries;
 import virtuoel.pehkui.api.ScaleType;
@@ -19,6 +20,8 @@ import virtuoel.pehkui.api.ScaleType;
 public abstract class InventoryScreenMixin
 {
 	@Unique private static final ThreadLocal<Map<ScaleType, ScaleData>> pehkui$SCALES = ThreadLocal.withInitial(Object2ObjectLinkedOpenHashMap::new);
+	@Unique private static final ScaleData pehkui$IDENTITY = ScaleData.Builder.create().build();
+	@Unique private static final ThreadLocal<Box> pehkui$BOX = new ThreadLocal<>();
 	
 	@Inject(method = "drawEntity", at = @At(value = "HEAD"))
 	private static void pehkui$drawEntity$head(int x, int y, int size, float mouseX, float mouseY, LivingEntity entity, CallbackInfo info)
@@ -32,8 +35,11 @@ public abstract class InventoryScreenMixin
 			cachedData = scales.computeIfAbsent(type, t -> ScaleData.Builder.create().build());
 			data = type.getScaleData(entity);
 			cachedData.fromScale(data, false);
-			data.resetScale(false);
+			data.fromScale(pehkui$IDENTITY, false);
 		}
+		
+		pehkui$BOX.set(entity.getBoundingBox());
+		entity.setBoundingBox(entity.getDimensions(entity.getPose()).getBoxAt(entity.getPos()));
 	}
 	
 	@Inject(method = "drawEntity", at = @At(value = "RETURN"))
@@ -45,5 +51,7 @@ public abstract class InventoryScreenMixin
 		{
 			type.getScaleData(entity).fromScale(scales.get(type), false);
 		}
+		
+		entity.setBoundingBox(pehkui$BOX.get());
 	}
 }
