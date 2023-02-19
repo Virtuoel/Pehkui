819b63dadeb30a40f53eaeb3e122b0d6aef597a6
Virtuoel <Virtuoel@users.noreply.github.com>
2023-02-19 15:07:31 +0300

    Scaled entity interaction fixes

--- src/main/java/virtuoel/pehkui/mixin/compat118minus/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/compat118minus/ServerPlayNetworkHandlerMixin.java
@@ -1,15 +1,12 @@
-package virtuoel.pehkui.mixin.compat119plus;
+package virtuoel.pehkui.mixin.compat118minus;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
 import virtuoel.pehkui.util.ScaleUtils;
@@ -17,31 +14,10 @@ import virtuoel.pehkui.util.ScaleUtils;
 @Mixin(ServerPlayNetworkHandler.class)
 public class ServerPlayNetworkHandlerMixin
 {
-	@Shadow ServerPlayerEntity player;
-	
-	@ModifyArg(method = "onPlayerInteractBlock", index = 0, at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Vec3d;squaredDistanceTo(Lnet/minecraft/util/math/Vec3d;)D"))
-	private Vec3d pehkui$onPlayerInteractBlock$center(Vec3d center)
+	@Redirect(method = "onPlayerInteractEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;squaredDistanceTo(Lnet/minecraft/entity/Entity;)D"))
+	private double pehkui$onPlayerInteractEntity$squaredDistanceTo(ServerPlayerEntity player, Entity entity)
 	{
-		final BlockPos pos = new BlockPos(center);
-		final Vec3d eye = player.getEyePos();
-		final BlockPos eyePos = new BlockPos(eye);
-		
-		final double xOffset = eye.getX() - center.getX();
-		final double yOffset = eye.getY() - center.getY();
-		final double zOffset = eye.getZ() - center.getZ();
-		
-		center = center.add(
-			eyePos.getX() == pos.getX() ? xOffset : xOffset < 0.0D ? -0.5D : 0.5D,
-			eyePos.getY() == pos.getY() ? yOffset : yOffset < 0.0D ? -0.5D : 0.5D,
-			eyePos.getZ() == pos.getZ() ? zOffset : zOffset < 0.0D ? -0.5D : 0.5D
-		);
-		
-		return center;
-	}
-	
-	@Redirect(method = "onPlayerInteractEntity", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/Entity;squaredDistanceTo(Lnet/minecraft/util/math/Vec3d;)D"))
-	private double pehkui$onPlayerInteractEntity$squaredDistanceTo(Entity entity, Vec3d eyePos)
-	{
+		final Vec3d eyePos = ScaleUtils.getEyePos(player);
 		final float margin = entity.getTargetingMargin();
 		
 		Box box = entity.getBoundingBox().expand(margin);
--- src/main/java/virtuoel/pehkui/util/ScaleUtils.java
+++ src/main/java/virtuoel/pehkui/util/ScaleUtils.java
@@ -22,6 +22,7 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
 import virtuoel.pehkui.Pehkui;
 import virtuoel.pehkui.api.PehkuiConfig;
 import virtuoel.pehkui.api.ScaleData;
@@ -261,6 +262,31 @@ public class ScaleUtils
 			packetSender.accept(new CustomPayloadS2CPacket(Pehkui.SCALE_PACKET, buffer));
 			syncedScales.clear();
 		}
+	}
+	
+	public static Vec3d getEyePos(final Entity entity)
+	{
+		return getEyePos(entity, Direction.DOWN);
+	}
+	
+	public static Vec3d getEyePos(final PlayerEntity entity)
+	{
+		return getEyePos(entity, GravityChangerCompatibility.INSTANCE.getGravityDirection(entity));
+	}
+	
+	public static Vec3d getEyePos(final Entity entity, final Direction gravity)
+	{
+		final double offsetX = entity.getStandingEyeHeight() * -gravity.getOffsetX();
+		final double offsetY = entity.getStandingEyeHeight() * -gravity.getOffsetY();
+		final double offsetZ = entity.getStandingEyeHeight() * -gravity.getOffsetZ();
+		final double footXCoord = entity.getPos().getX();
+		final double footYCoord = entity.getPos().getY();
+		final double footZCoord = entity.getPos().getZ();
+		final double headXCoord = footXCoord + offsetX;
+		final double headYCoord = footYCoord + offsetY;
+		final double headZCoord = footZCoord + offsetZ;
+		
+		return new Vec3d(headXCoord, headYCoord, headZCoord);
 	}
 	
 	public static double getBlockXOffset(BlockPos pos, PlayerEntity player)
