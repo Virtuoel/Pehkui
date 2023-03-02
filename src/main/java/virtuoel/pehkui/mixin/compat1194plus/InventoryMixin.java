2878ec798923c69081c5d7643bc5e8c3db8a4a7a
Virtuoel <Virtuoel@users.noreply.github.com>
2023-03-02 23:43:28 +0300

    Fix reach being slightly misaligned

--- src/main/java/virtuoel/pehkui/mixin/compat1194plus/InventoryMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/compat1194plus/InventoryMixin.java
@@ -18,23 +18,25 @@ public interface InventoryMixin
 	public static boolean canPlayerUse(BlockEntity blockEntity, PlayerEntity player, int range)
 	{
 		World world = blockEntity.getWorld();
-		BlockPos blockPos = blockEntity.getPos();
+		BlockPos pos = blockEntity.getPos();
 		
 		if (world == null)
 		{
 			return false;
 		}
-		else if (world.getBlockEntity(blockPos) != blockEntity)
+		else if (world.getBlockEntity(pos) != blockEntity)
 		{
 			return false;
 		}
 		else
 		{
-			final BlockPos pos = blockEntity.getPos();
+			double x = ((double) pos.getX()) + 0.5D;
+			double y = ((double) pos.getY()) + 0.5D;
+			double z = ((double) pos.getZ()) + 0.5D;
 			final Vec3d eyePos = player.getEyePos();
-			final double x = ((double) pos.getX()) + 0.5D + ScaleUtils.getBlockXOffset(pos, player) - (eyePos.getX() - player.getX());
-			final double y = ((double) pos.getY()) + 0.5D + ScaleUtils.getBlockYOffset(pos, player) - (eyePos.getY() - player.getY());
-			final double z = ((double) pos.getZ()) + 0.5D + ScaleUtils.getBlockZOffset(pos, player) - (eyePos.getZ() - player.getZ());
+			x = (x - 0.5D) + ScaleUtils.getBlockXOffset(pos, player) - (eyePos.getX() - player.getX());
+			y = (y - 0.5D) + ScaleUtils.getBlockYOffset(pos, player) - (eyePos.getY() - player.getY());
+			z = (z - 0.5D) + ScaleUtils.getBlockZOffset(pos, player) - (eyePos.getZ() - player.getZ());
 			final double reach = ScaleUtils.getBlockReachScale(player) * (double) range;
 			return player.squaredDistanceTo(x, y, z) <= reach * reach;
 		}
