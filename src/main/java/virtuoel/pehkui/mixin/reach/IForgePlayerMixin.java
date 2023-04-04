85c2104a577e0162052df4cf4b219e8f67d1d60a
Virtuoel <Virtuoel@users.noreply.github.com>
2023-04-05 01:21:04 +0300

    Update deps

--- src/main/java/virtuoel/pehkui/mixin/reach/IForgePlayerMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/reach/IForgePlayerMixin.java
@@ -12,6 +12,31 @@ import virtuoel.pehkui.util.ScaleUtils;
 public interface IForgePlayerMixin
 {
 	@Overwrite(remap = false)
+	default double getEntityReach()
+	{
+		final PlayerEntity self = ((PlayerEntity) this);
+		final double range = self.getAttributeValue(ForgeMod.ENTITY_REACH.get());
+		final double ret = range == 0.0 ? 0.0 : range + (double) (self.isCreative() ? 3 : 0);
+		
+		final float scale = ScaleUtils.getEntityReachScale(self);
+		
+		return scale != 1.0F ? ret * scale : ret;
+	}
+	
+	@Overwrite(remap = false)
+	default double getBlockReach()
+	{
+		final PlayerEntity self = ((PlayerEntity) this);
+		final double reach = self.getAttributeValue(ForgeMod.BLOCK_REACH.get());
+		final double ret = reach == 0.0 ? 0.0 : reach + (self.isCreative() ? 0.5 : 0.0);
+		
+		final float scale = ScaleUtils.getBlockReachScale(self);
+		
+		return scale != 1.0F ? ret * scale : ret;
+	}
+	
+	@Deprecated
+	@Overwrite(remap = false)
 	default double getAttackRange()
 	{
 		final PlayerEntity self = ((PlayerEntity) this);
@@ -23,6 +48,7 @@ public interface IForgePlayerMixin
 		return scale != 1.0F ? ret * scale : ret;
 	}
 	
+	@Deprecated
 	@Overwrite(remap = false)
 	default double getReachDistance()
 	{
