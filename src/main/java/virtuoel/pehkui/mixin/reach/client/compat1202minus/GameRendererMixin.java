68e6a29190c98f2e3ccd2591a7c37437bad14b7e
Virtuoel <Virtuoel@users.noreply.github.com>
2024-02-17 03:28:30 +0300

    Use ModifyExpressionValue instead of ModifyConstant
    Fixes #485

--- src/main/java/virtuoel/pehkui/mixin/reach/client/compat1202minus/GameRendererMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/reach/client/compat1202minus/GameRendererMixin.java
@@ -4,8 +4,9 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.Constant;
-import org.spongepowered.asm.mixin.injection.ModifyConstant;
+import org.spongepowered.asm.mixin.injection.At;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.GameRenderer;
@@ -18,7 +19,7 @@ public class GameRendererMixin
 	@Shadow @Final @Mutable
 	MinecraftClient client;
 	
-	@ModifyConstant(method = "updateTargetedEntity", constant = @Constant(doubleValue = 3.0D))
+	@ModifyExpressionValue(method = "updateTargetedEntity", at = @At(value = "CONSTANT", args = "doubleValue=3.0D"))
 	private double pehkui$updateTargetedEntity$distance(double value, float tickDelta)
 	{
 		final Entity entity = client.getCameraEntity();
