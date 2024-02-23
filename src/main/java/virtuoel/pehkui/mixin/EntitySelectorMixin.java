e6303c21fe11cc67170471f78dc6b5336672550a
Virtuoel <Virtuoel@users.noreply.github.com>
2024-02-24 00:28:30 +0300

    Convert to ModifyArg

--- src/main/java/virtuoel/pehkui/mixin/EntitySelectorMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/EntitySelectorMixin.java
@@ -2,9 +2,8 @@ package virtuoel.pehkui.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArg;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.llamalad7.mixinextras.sugar.Local;
 
 import net.minecraft.command.EntitySelector;
@@ -15,8 +14,8 @@ import virtuoel.pehkui.util.ScaleUtils;
 @Mixin(EntitySelector.class)
 public class EntitySelectorMixin
 {
-	@WrapOperation(method = "method_9810", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Box;intersects(Lnet/minecraft/util/math/Box;)Z"))
-	private static boolean pehkui$method_9810$intersects(Box bounds, Operation<Boolean> original, @Local(argsOnly = true) Entity obj)
+	@ModifyArg(method = "method_9810", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/math/Box;intersects(Lnet/minecraft/util/math/Box;)Z"))
+	private static Box pehkui$method_9810$intersects(Box bounds, @Local(argsOnly = true) Entity obj)
 	{
 		final float interactionWidth = ScaleUtils.getInteractionBoxWidthScale(obj);
 		final float interactionHeight = ScaleUtils.getInteractionBoxHeightScale(obj);
@@ -27,9 +26,9 @@ public class EntitySelectorMixin
 			final double scaledYLength = bounds.getLengthY() * 0.5D * (interactionHeight - 1.0F);
 			final double scaledZLength = bounds.getLengthZ() * 0.5D * (interactionWidth - 1.0F);
 			
-			bounds = bounds.expand(scaledXLength, scaledYLength, scaledZLength);
+			return bounds.expand(scaledXLength, scaledYLength, scaledZLength);
 		}
 		
-		return original.call(bounds);
+		return bounds;
 	}
 }
