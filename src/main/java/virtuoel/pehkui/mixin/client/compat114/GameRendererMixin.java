c09b57b0f4c7ffa8fafd62e8c2987d12aefe621a
Virtuoel <Virtuoel@users.noreply.github.com>
2024-03-15 01:17:04 +0300

    Consistent bobbing for 1.14.4 mixin

--- src/main/java/virtuoel/pehkui/mixin/client/compat114/GameRendererMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/client/compat114/GameRendererMixin.java
@@ -1,21 +1,22 @@
 package virtuoel.pehkui.mixin.client.compat114;
 
-import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Dynamic;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.GameRenderer;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.util.math.MathHelper;
 import virtuoel.pehkui.util.MixinConstants;
 import virtuoel.pehkui.util.ScaleRenderUtils;
 import virtuoel.pehkui.util.ScaleUtils;
@@ -33,22 +34,40 @@ public class GameRendererMixin
 		return ScaleRenderUtils.modifyProjectionMatrixDepthByWidth(value, client.getCameraEntity(), client.getTickDelta());
 	}
 	
+	@Unique
+	boolean pehkui$isBobbing = false;
+	
 	@Dynamic
-	@Inject(method = MixinConstants.APPLY_CAMERA_TRANSFORMATIONS, at = @At(value = "INVOKE", target = MixinConstants.BOB_VIEW))
-	private void pehkui$renderWorld(float tickDelta, CallbackInfo info)
+	@Inject(method = MixinConstants.APPLY_CAMERA_TRANSFORMATIONS, at = @At(value = "INVOKE", shift = Shift.BEFORE, target = MixinConstants.BOB_VIEW))
+	private void pehkui$renderWorld$before(float tickDelta, CallbackInfo info)
 	{
-		final float scale = ScaleUtils.getViewBobbingScale(client.getCameraEntity(), client.getTickDelta());
-		
-		if (scale != 1.0F)
+		pehkui$isBobbing = true;
+	}
+	
+	@Dynamic
+	@Inject(method = MixinConstants.APPLY_CAMERA_TRANSFORMATIONS, at = @At(value = "INVOKE", shift = Shift.AFTER, target = MixinConstants.BOB_VIEW))
+	private void pehkui$renderWorld$after(float tickDelta, CallbackInfo info)
+	{
+		pehkui$isBobbing = false;
+	}
+	
+	@Dynamic
+	@WrapOperation(method = MixinConstants.BOB_VIEW, at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/platform/GlStateManager;translatef(FFF)V", remap = false))
+	private void pehkui$bobView$translate(float x, float y, float z, Operation<Void> original)
+	{
+		if (pehkui$isBobbing)
 		{
-			final float multiplier = scale - 1.0F;
+			final float scale = ScaleUtils.getViewBobbingScale(client.getCameraEntity(), client.getTickDelta());
 			
-			final PlayerEntity playerEntity = (PlayerEntity) client.getCameraEntity();
-			final float speedLerp = -(playerEntity.horizontalSpeed + ((playerEntity.horizontalSpeed - playerEntity.prevHorizontalSpeed) * tickDelta));
-			final float strideLerp = MathHelper.lerp(tickDelta, playerEntity.prevStrideDistance, playerEntity.strideDistance);
-			
-			GL11.glTranslatef(multiplier * MathHelper.sin(speedLerp * (float) Math.PI) * strideLerp * 0.5F, multiplier * -Math.abs(MathHelper.cos(speedLerp * (float) Math.PI) * strideLerp), 0.0F);
+			if (scale != 1.0F)
+			{
+				x *= scale;
+				y *= scale;
+				z *= scale;
+			}
 		}
+		
+		original.call(x, y, z);
 	}
 	
 	@Dynamic
